d60ca7ee2f34cad9d9af163676a5d5bb417f03d6
Prathap Chandran <notprathap@gmail.com>
2014-07-10 22:26:05 +0100

    generating shapes based on the number

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -18,14 +18,20 @@ public class PlayerController : MonoBehaviour
 		count = 0;
 		SetCountText ();
 		winText.text = "";
+		int randomNumber = 0;
+		for (int i = 0; i < 8; i++) {
+			randomNumber = Random.Range(0, 9999);
+			if ((randomNumber >= 0) && (randomNumber < 10) ){
+				Instantiate (sphere, new Vector3 (Random.Range(-10,10), 0.5f, Random.Range(-10,10)), Quaternion.identity);
+			} else if ((randomNumber >= 10) && (randomNumber < 99) ){
+				Instantiate (twoSidedCube, new Vector3 (Random.Range(-10,10), 0.5f, Random.Range(-10,10)), Quaternion.identity);
+			} else if ((randomNumber >= 100) && (randomNumber < 999) ){
+				Instantiate (threeSidedShape, new Vector3 (Random.Range(-10,10), 0.5f, Random.Range(-10,10)), Quaternion.identity);
+			} else {
+				Instantiate (fourSidedCube, new Vector3 (Random.Range(-10,10), 0.5f, Random.Range(-10,10)), Quaternion.identity);
+			}
 
-		//Instantiate (pickUpPrefab, new Vector3 (-5f, 0.5f, 0), Quaternion.identity);
-		Instantiate (sphere, new Vector3 (Random.Range(-10,10), 0.5f, Random.Range(-10,10)), Quaternion.identity);
-		Instantiate (twoSidedCube, new Vector3 (Random.Range(-10,10), 0.5f, Random.Range(-10,10)), Quaternion.identity);
-		Instantiate (threeSidedShape, new Vector3 (Random.Range(-10,10), 0.5f, Random.Range(-10,10)), Quaternion.identity);
-		Instantiate (threeSidedShape, new Vector3 (Random.Range(-10,10), 0.5f, Random.Range(-10,10)), Quaternion.identity);
-		Instantiate (fourSidedCube, new Vector3 (Random.Range(-10,10), 0.5f, Random.Range(-10,10)), Quaternion.identity);
-		Instantiate (fourSidedCube, new Vector3 (Random.Range(-10,10), 0.5f, Random.Range(-10,10)), Quaternion.identity);
+		}
 	}
 
 	// Update is called once per frame
@@ -54,4 +60,5 @@ public class PlayerController : MonoBehaviour
 			winText.text = "You Win!";
 		}
 	}
+
 }
